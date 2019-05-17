db48bb9e5f6f7cc1abdf190a1eadc5c6a9a4b1f4
Jerson M <jerson.mari@gmail.com>
2019-05-17 19:52:07 +0800

    Setup models for db tables

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class PizzaTopping(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.name}"
+
+class Pasta(models.Model):
+    name = models.CharField(max_length=64)
+    price = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.name} - ${self.price}"
+
+class Salad(models.Model):
+    name = models.CharField(max_length=64)
+    price = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.name} - ${self.price}"
+
+class DinnerPlatter(models.Model):
+    name = models.CharField(max_length=64)
+    small_price = models.IntegerField()
+    large_price = models.IntegerField()
