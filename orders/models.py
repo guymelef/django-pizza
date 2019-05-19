13ebff37ec2bd57b9c0fe7203cc38729687b06ea
Jerson M <jerson.mari@gmail.com>
2019-05-19 19:03:25 +0800

    Edit db models

--- orders/models.py
+++ orders/models.py
@@ -17,6 +17,12 @@ class SicilianPizza(models.Model):
     def __str__(self):
         return f"{self.name} - Small(${self.small}) | Large(${self.large})"
 
+class PizzaTopping(models.Model):
+    name = models.CharField(max_length=120, unique=True)
+
+    def __str__(self):
+        return f"{self.name}"
+        
 class SubExtra(models.Model):
     name = models.CharField(max_length=120, unique=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
@@ -24,20 +30,14 @@ class SubExtra(models.Model):
     def __str__(self):
         return f"{self.name} - ${self.price}"
 
-class Subs(models.Model):
+class Sub(models.Model):
     name = models.CharField(max_length=120, unique=True)
     small = models.DecimalField(max_digits=10, decimal_places=2)
     large = models.DecimalField(max_digits=10, decimal_places=2)
-    addOns = models.ManyToManyField(SubExtra, blank=True, related_name="extras")
+    add_Ons = models.ManyToManyField(SubExtra, blank=True, related_name="extras")
 
     def __str__(self):
         return f"{self.name} - Small(${self.small}) | Large(${self.large})"
-
-class PizzaTopping(models.Model):
-    name = models.CharField(max_length=120, unique=True)
-
-    def __str__(self):
-        return f"{self.name}"
 
 class Pasta(models.Model):
     name = models.CharField(max_length=120, unique=True)
