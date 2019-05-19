75b8cc7671030de1d7a55ee9c17305328baec7c7
Jerson M <jerson.mari@gmail.com>
2019-05-19 19:04:06 +0800

    Register all db tables

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from .models import RegularPizza, SicilianPizza, PizzaTopping, SubExtra, Sub, Pasta, Salad, DinnerPlatter
+
 # Register your models here.
+admin.site.register(RegularPizza)
+admin.site.register(SicilianPizza)
+admin.site.register(PizzaTopping)
+admin.site.register(SubExtra)
+admin.site.register(Sub)
+admin.site.register(Pasta)
+admin.site.register(Salad)
+admin.site.register(DinnerPlatter)
